4b146f7f79f28d2c7ea37fcda391fd06f6b33e1c
MitchGMcKinney <mgmckinn2325@me.com>
2022-04-28 21:31:33 -0500

    trying to get server to connect inquirer not working

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,30 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;
-CREATE TABLE department (
-  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  dept_name VARCHAR(30),
-  
-);
-CREATE TABLE role (
-  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL (10, 2),
-  department_id INTEGER NOT NULL,
-  
-  CONSTRAINT fk_department
-  FOREIGN KEY (department_id)
-  REFERENCES department(id) ON DELETE SET NULL 
-);
+
 CREATE TABLE employee (
   id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  role_id INTEGER(8),
-  manager_id INTEGER(8),
-  CONSTRAINT fk_role 
-  FOREIGN KEY (role_id) 
-  REFERENCES role(id) ON DELETE SET NULL 
+  role_id INTEGER,
+  manager_id INTEGER
+ 
+); 
+
+CREATE TABLE department (
+  department_id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(45) NOT NULL,
+  PRIMARY KEY (department_id)
+ 
+  
 );
+  
+CREATE TABLE role (
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  role_name VARCHAR(30),
+  salary DECIMAL(10,2),
+  department_id INTEGER
+
+); 
+
+
